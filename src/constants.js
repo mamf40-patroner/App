8ad6869ab91f3b0ff6aa85b45489109ca39e693d
Elliott Linder <elliott.darfink@gmail.com>
2019-02-11 11:27:05 +0100

    feat: add POI constants

---
+++ src/constants.js
@@ -0,0 +1,61 @@
+export const categories = [
+  {
+    "name": "Boende",
+    "subcategories": [
+      { "name": "Campingplats", "id": 103 },
+    ],
+  },
+  {
+    "name": "Konst & Kultur",
+    "id": 130,
+  },
+  {
+    "name": "Historia",
+    "id": 220,
+  },
+  {
+    "name": "Underhållning",
+    "subcategories": [
+      { "name": "Badplats",       "id": 263 },
+      { "name": "Skridskobana",   "id": 276 },
+      { "name": "Minigolf",       "id": 278 },
+      { "name": "Naturreservat",  "id": 279 },
+      { "name": "Park",           "id": 280 },
+      { "name": "Picknickplats",  "id": 281 },
+      { "name": "Offentligt bad", "id": 285 },
+      { "name": "Badområde",      "id": 291 },
+      { "name": "Simbassäng",     "id": 292 },
+      { "name": "Zoo",            "id": 310 },
+    ],
+  },
+  {
+    "name": "Turism",
+    "id": 620,
+  },
+  {
+    "name": "Natur",
+    "subcategories": [
+      { "name": "Strand", "id": 332 },
+    ],
+  },
+  {
+    "name": "Transport",
+    "subcategories": [
+      { "name": "Cykelverkstad", "id": 585 },
+    ],
+  },
+  {
+    "name": "Matplatser",
+    "subcategories": [
+      { "name": "Bar",          "id": 561 },
+      { "name": "Barbeque",     "id": 562 },
+      { "name": "Café",         "id": 564 },
+      { "name": "Dricksvatten", "id": 565 },
+      { "name": "Snabbmat",     "id": 566 },
+      { "name": "Food court",   "id": 567 },
+      { "name": "Glass",        "id": 568 },
+      { "name": "Pub",          "id": 569 },
+      { "name": "Restaurant",   "id": 570 },
+    ],
+  },
+]
