fd92048e68ecb7f22bf77b2a4fcab00c126b3c0a
Elliott Linder <elliott.darfink@gmail.com>
2019-02-23 15:40:17 +0100

    fix: use axios for 'getDirections'

--- src/api.js
+++ src/api.js
@@ -2,10 +2,18 @@ import { OPENROUTESERVICE_KEY } from 'react-native-dotenv';
 import axios from 'axios';
 import R from 'ramda';
 
+const MAX_RANGE = 2000;
+
+const ORS = axios.create({
+  baseURL: 'https://api.openrouteservice.org',
+  params: {
+    api_key: OPENROUTESERVICE_KEY
+  }
+});
+
 async function getBoundingBox({ longitude, latitude }, range) {
-  return axios.get('https://api.openrouteservice.org/isochrones', {
+  return ORS.get('isochrones', {
     params: {
-      api_key: OPENROUTESERVICE_KEY,
       locations: `${longitude},${latitude}`,
       profile: 'cycling-regular',
       range_type: 'distance',
@@ -14,45 +22,39 @@ async function getBoundingBox({ longitude, latitude }, range) {
   }).then(({ data }) => R.splitEvery(2, data.bbox))
 }
 
-export async function getPois(coordinate, types = [], range = 2000) {
-  const bbox = await getBoundingBox(coordinate, Math.min(range, 2000));
+export async function getPois(coordinate, types = [], range = MAX_RANGE) {
+  const bbox = await getBoundingBox(coordinate, Math.min(range, MAX_RANGE));
 
   const isGroupCategory = type => type % 10 == 0;
   const groups = types.filter(isGroupCategory);
   const categories = types.filter(R.complement(isGroupCategory));
 
-  const filters = {}
-  if (groups.length > 0) {
-    filters.category_group_ids = groups;
-  }
-
-  if (categories.length > 0) {
-    filters.category_ids = categories;
-  }
-
-  return axios.post(`https://api.openrouteservice.org/pois?api_key=${OPENROUTESERVICE_KEY}`, {
+  return ORS.post('pois', {
     request: 'pois',
     geometry: { bbox },
-    filters,
+    filters: {
+      ...(groups.length > 0 ? { category_group_ids: groups } : {}),
+      ...(categories.length > 0 ? { category_ids: categories } : {}),
+    },
   }).then(({ data }) => data.features);
 }
 
 export async function getDirections(coordinates) {
-  const api = new Directions({ api_key: OPENROUTESERVICE_KEY });
-
-  return api.calculate({
-    coordinates,
-    profile: 'cycling-regular',
-    geometry_format: 'polyline',
-    format: 'json'
-  }).then(path => {
-    console.log(JSON.stringify(path));
-  });
+  return ORS.get('directions', {
+    params: {
+      coordinates: coordinates
+        .map(({ longitude, latitude }) => `${longitude},${latitude}`)
+        .join('|'),
+      profile: 'cycling-regular',
+      geometry_format: 'polyline',
+      format: 'json'
+    }
+  }).then(R.prop('data'));
 }
 
 export async function getCurrentLocation() {
   return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(function({ coords }) {
+    navigator.geolocation.getCurrentPosition(function ({ coords }) {
       resolve(coords);
     }, reject);
   });
